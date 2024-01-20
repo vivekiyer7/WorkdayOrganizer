404ee431da3c522822c133cf109c42f8206cfa2c
Vivek Iyer <vivek370@gmail.com>
2024-01-20 11:24:21 +0000

    Basic Structure and Logic of Color

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
-    
-    // Update the current day using Day.js
-    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
-});
+  // Update the current day using Day.js
+  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'))
+
+  function generateTimeBlocks() {
+    const container = $(".container");
+    const workHours = [
+      "9 AM", "10 AM", "11 AM",
+      "12 PM", "1 PM", "2 PM",
+      "3 PM", "4 PM", "5 PM"
+    ];
+  
+    // Get the current hour using Day.js
+    const currentHour = dayjs().hour();
+  
+    // Loop through work hours and create time blocks
+    workHours.forEach((hour, index) => {
+      const timeBlock = $("<div>").addClass("row time-block");
+      const hourCol = $("<div>").addClass("col-md-1 hour").text(hour);
+      const textAreaCol = $("<textarea>").addClass("col-md-10 description");
+  
+      // Set the background color based on past, present, or future
+      if (index + 9 < currentHour) {
+        textAreaCol.addClass("past");
+      } else if (index + 9 === currentHour) {
+        textAreaCol.addClass("present");
+      } else {
+        textAreaCol.addClass("future");
+      }
+  
+      const saveBtnCol = $("<div>").addClass("col-md-1 saveBtn");
+      timeBlock.append(hourCol, textAreaCol, saveBtnCol);
+
+      // Append the time block to the container
+      container.append(timeBlock);
+    });
+  }
+
+  generateTimeBlocks();
+})
