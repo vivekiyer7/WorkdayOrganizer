e1800581a8c6348a7cd921dd5d03fbd09ff1590f
Vivek Iyer <vivek370@gmail.com>
2024-01-20 11:06:00 +0000

    Corrected Display Time using day.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  //Display current date in format Thrusday, September 5th
-  const currentDateEl = document.getElementById('currentDay')
-  const currentDate = new Date()
-  const currentDateString = currentDate.toLocaleDateString('en-US', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric'
-  })
-  currentDateEl.textContent = currentDateString
+$(document).ready(function () {
+    
+    // Update the current day using Day.js
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
