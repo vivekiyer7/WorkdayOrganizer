3a1167e32a2819a49678080a8e8a98be10fd59b9
Vivek Iyer <vivek370@gmail.com>
2024-01-20 11:28:44 +0000

    Save and Retrieve from Local Storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,63 +1,63 @@
 $(document).ready(function () {
   // Update the current day using Day.js
-  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'))
 
-  function generateTimeBlocks() {
-    const container = $(".container");
+  function generateTimeBlocks () {
+    const container = $('.container')
     const workHours = [
-      "9 AM",
-      "10 AM",
-      "11 AM",
-      "12 PM",
-      "1 PM",
-      "2 PM",
-      "3 PM",
-      "4 PM",
-      "5 PM",
-    ];
+      '9 AM',
+      '10 AM',
+      '11 AM',
+      '12 PM',
+      '1 PM',
+      '2 PM',
+      '3 PM',
+      '4 PM',
+      '5 PM'
+    ]
 
     // Get the current hour using Day.js
-    const currentHour = dayjs().hour();
+    const currentHour = dayjs().hour()
 
     // Loop through work hours and create time blocks
     workHours.forEach((hour, index) => {
-      const timeBlock = $("<div>").addClass("row time-block");
-      const hourCol = $("<div>").addClass("col-md-1 hour").text(hour);
-      const textAreaCol = $("<textarea>").addClass("col-md-10 description");
+      const timeBlock = $('<div>').addClass('row time-block')
+      const hourCol = $('<div>').addClass('col-md-1 hour').text(hour)
+      const textAreaCol = $('<textarea>').addClass('col-md-10 description')
 
       // Set the background color based on past, present, or future
       if (index + 9 < currentHour) {
-        textAreaCol.addClass("past");
+        textAreaCol.addClass('past')
       } else if (index + 9 === currentHour) {
-        textAreaCol.addClass("present");
+        textAreaCol.addClass('present')
       } else {
-        textAreaCol.addClass("future");
+        textAreaCol.addClass('future')
       }
 
       // Load saved event from local storage
-      const savedEvent = localStorage.getItem(hour);
+      const savedEvent = localStorage.getItem(hour)
       if (savedEvent) {
-        textAreaCol.val(savedEvent);
+        textAreaCol.val(savedEvent)
       }
 
-      const saveBtnCol = $("<div>").addClass("col-md-1 saveBtn");
-      const saveBtn = $("<i>").addClass("fas fa-save");
+      const saveBtnCol = $('<div>').addClass('col-md-1 saveBtn')
+      const saveBtn = $('<i>').addClass('fas fa-save')
 
       // Save button click event
-      saveBtn.on("click", function () {
-        const eventText = textAreaCol.val();
-        localStorage.setItem(hour, eventText);
-      });
+      saveBtn.on('click', function () {
+        const eventText = textAreaCol.val()
+        localStorage.setItem(hour, eventText)
+      })
 
-      saveBtnCol.append(saveBtn);
+      saveBtnCol.append(saveBtn)
 
       // Append elements to the time block
-      timeBlock.append(hourCol, textAreaCol, saveBtnCol);
+      timeBlock.append(hourCol, textAreaCol, saveBtnCol)
 
       // Append the time block to the container
-      container.append(timeBlock);
-    });
+      container.append(timeBlock)
+    })
   }
 
-  generateTimeBlocks();
-});
+  generateTimeBlocks()
+})
